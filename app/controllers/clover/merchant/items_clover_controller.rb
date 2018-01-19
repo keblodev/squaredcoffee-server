198c20c806becdd54391772f8c647877d0a0c87b
Ronan Amsterdam <romanfromrome.com@gmail.com>
2018-01-19 23:04:30 +0800

    shop deletion edgecases covered

--- app/controllers/clover/merchant/items_clover_controller.rb
+++ app/controllers/clover/merchant/items_clover_controller.rb
@@ -12,26 +12,31 @@ class Clover::Merchant::ItemsCloverController < ApplicationController
     # 1.all in one call
     def get_all_categories_with_items
         if shop = get_shop(params[:id])
-        remote_id       = shop.remote_id
-        access_token    = shop.token
+            remote_id       = shop.remote_id
+            access_token    = shop.token
 
-        begin
-            puts "Getting categories for remote_id: #{remote_id}"
-            cat_req = HTTP
-            .headers(:authorization => "Bearer #{access_token}")
-            .get("#{@clover_base_api_url}/v3/merchants/#{remote_id}/categories?expand=items.modifierGroups.modifiers,items.taxRates")
-            cat_resp    = cat_req.parse
-            categories  = cat_resp["elements"]
-        rescue HTTP::ResponseError => e
-            raise "Error on get_all_categories_with_items: #{e.message}"
+            if is_rmote_shop_authorized(remote_id, access_token)
+                begin
+                    puts "Getting categories for remote_id: #{remote_id}"
+                    cat_req = HTTP
+                    .headers(:authorization => "Bearer #{access_token}")
+                    .get("#{@clover_base_api_url}/v3/merchants/#{remote_id}/categories?expand=items.modifierGroups.modifiers,items.taxRates")
+                    cat_resp    = cat_req.parse
+                    categories  = cat_resp["elements"]
+                rescue HTTP::ResponseError => e
+                    raise "Error on get_all_categories_with_items: #{e.message}"
 
-            render :json => {:error => JSON.parse(e.response_body)["errors"]}, :status => 400
-            return
-        end
+                    render :json => {:error => JSON.parse(e.response_body)["errors"]}, :status => 400
+                    return
+                end
 
-        render json: {:status => 200, :data => {data: categories, id: params[:id]}}
+                render json: {:status => 200, :data => {data: categories, id: params[:id]}}
+            else
+                render :json => {:message => "shop not authorized", reset_app_state: true}, :status => 409
+            end
+
         else
-            render :json => {:message => "shop not found"}, :status => 404
+            render :json => {:message => "shop not found", reset_app_state: true}, :status => 404
         end
     end
 
@@ -59,31 +64,34 @@ class Clover::Merchant::ItemsCloverController < ApplicationController
         render json: {:status => 200, :data => {items: items}}
     end
 
-    def get_with_modifiers
-        # {{url}}/v3/merchants/{{mId}}?access_token={{token}}
-        shop = get_shop(params[:id])
-
-        item_id = params[:item_id]
-
-        begin
-        	puts "Getting item for remote_id: #{remote_id}"
-            item_request = HTTP
-            .headers(:authorization => "Bearer #{access_token}")
-            .get("#{@clover_base_api_url}/v3/merchants/#{remote_id}/items/#{item_id}?expand=modifierGroups.modifiers")
-            item_resp    = item_request.parse
-            item  = item_resp["elements"]
-        rescue HTTP::ResponseError => e
-            raise "Error on get_item: #{e.message}"
-
-            render :json => {:error => JSON.parse(e.response_body)["errors"]}, :status => 400
-        	return
-        end
-
-        render json: {:status => 200, :data => {item: item}}
-    end
-
     def get_shop(remote_id)
         Shop.find_by_remote_id(remote_id)
+    end
+
+    def is_rmote_shop_authorized(shop_id, access_token)
+        is_rmote_shop_authorized = true
+        begin
+            puts "Getting info for shop_id: #{shop_id}"
+            merchant_request = HTTP
+            .headers(:authorization => "Bearer #{access_token}")
+            .get("#{@clover_base_api_url}/v3/merchants/#{shop_id}?expand=address,openingHours")
+            merchant_resp = merchant_request.parse
+            if merchant_request.status == 401
+                if rejected_shop = Shop.find_by_remote_id(shop_id)
+                    is_rmote_shop_authorized = false
+                    Shop.delete(rejected_shop.id)
+                end
+            end
+
+        rescue HTTP::ResponseError => e
+            raise "Error encountered while getting access token: #{e.message}"
+
+            render :json => {:error => JSON.parse(e.response_body)["errors"]}, :status => 400
+            is_rmote_shop_authorized = false
+            is_rmote_shop_authorized
+        end
+
+        is_rmote_shop_authorized
     end
 end
 # get categories
--- app/controllers/clover/merchant/pay_clover_controller.rb
+++ app/controllers/clover/merchant/pay_clover_controller.rb
@@ -7,6 +7,11 @@ require 'base64'
 
 class Clover::Merchant::PayCloverController < ApplicationController
 
+    def initialize
+        @clover_base_url         = Rails.application.secrets.clover_base_url
+        @clover_base_api_url     = Rails.application.secrets.clover_base_api_url
+    end
+
     def pay
         ###############################################
         ########## BEGIN SCRIPT CONFIG SETUP ##########
@@ -23,91 +28,95 @@ class Clover::Merchant::PayCloverController < ApplicationController
         shop_id         = shop.remote_id
         access_token    = shop.token
 
-        merchant_id     = params[:merchant_id] # sandbox Test Merchant
-        pay_api_url      = clover_pay_api_url #pay api is only here ..?
-        amount          = [payConfig["total"].to_i, 50].max
-        # tip_amount = 0
-        # tax_amount = 0
-        card_number     = payConfig["number"].to_s
-        first6          = payConfig["number"].to_s.first(6)
-        last4           = payConfig["number"].to_s.last(4)
-        exp_month       = payConfig["expiry"].to_i/100
-        exp_year        = ("20"+(payConfig["expiry"].to_i%100).to_s)
-        cvv             = payConfig["cvc"]
+        if is_rmote_shop_authorized(shop_id, access_token)
+            merchant_id     = params[:merchant_id] # sandbox Test Merchant
+            pay_api_url      = clover_pay_api_url #pay api is only here ..?
+            amount          = [payConfig["total"].to_i, 50].max
+            # tip_amount = 0
+            # tax_amount = 0
+            card_number     = payConfig["number"].to_s
+            first6          = payConfig["number"].to_s.first(6)
+            last4           = payConfig["number"].to_s.last(4)
+            exp_month       = payConfig["expiry"].to_i/100
+            exp_year        = ("20"+(payConfig["expiry"].to_i%100).to_s)
+            cvv             = payConfig["cvc"]
 
-        ###############################################
-        ########## END SCRIPT CONFIG SETUP ############
-        ###############################################
+            ###############################################
+            ########## END SCRIPT CONFIG SETUP ############
+            ###############################################
 
-        # GET to /v2/merchant/{mId}/pay/key To get the encryption information needed for the pay endpoint.
-        begin
-            puts "Getting key for order #{order_id} for shop_id: #{shop_id}"
-            key_order_request = HTTP
-                .headers(
-                    :authorization => "Bearer #{access_token}",
-                )
-                .get("#{pay_api_url}/#{merchant_id}/pay/key")
-            key_response = key_order_request.parse
-        rescue HTTP::ResponseError => e
-            raise "Error on new order: #{e.message}"
-            render :json => {:error => JSON.parse(e.response_body)["errors"]}, :status => 400
-            return
-        end
+            # GET to /v2/merchant/{mId}/pay/key To get the encryption information needed for the pay endpoint.
+            begin
+                puts "Getting key for order #{order_id} for shop_id: #{shop_id}"
+                key_order_request = HTTP
+                    .headers(
+                        :authorization => "Bearer #{access_token}",
+                    )
+                    .get("#{pay_api_url}/#{merchant_id}/pay/key")
+                key_response = key_order_request.parse
+            rescue HTTP::ResponseError => e
+                raise "Error on new order: #{e.message}"
+                render :json => {:error => JSON.parse(e.response_body)["errors"]}, :status => 400
+                return
+            end
 
-        modulus     = key_response["modulus"].to_i
-        exponent    = key_response["exponent"].to_i
-        prefix      = key_response["prefix"]
+            modulus     = key_response["modulus"].to_i
+            exponent    = key_response["exponent"].to_i
+            prefix      = key_response["prefix"]
 
-        # construct an RSA public key using the modulus and exponent provided by GET /v2/merchant/{mId}/pay/key
-        rsa = OpenSSL::PKey::RSA.new.tap do |rsa|
-            rsa.e = OpenSSL::BN.new(exponent)
-            rsa.n = OpenSSL::BN.new(modulus)
-        end
+            # construct an RSA public key using the modulus and exponent provided by GET /v2/merchant/{mId}/pay/key
+            rsa = OpenSSL::PKey::RSA.new.tap do |rsa|
+                rsa.e = OpenSSL::BN.new(exponent)
+                rsa.n = OpenSSL::BN.new(modulus)
+            end
 
-        # create a cipher from the RSA key and use it to encrypt the card number, prepended with the prefix from GET /v2/merchant/{mId}/pay/key
-        encrypted = rsa.public_encrypt(prefix + card_number, OpenSSL::PKey::RSA::PKCS1_OAEP_PADDING)
+            # create a cipher from the RSA key and use it to encrypt the card number, prepended with the prefix from GET /v2/merchant/{mId}/pay/key
+            encrypted = rsa.public_encrypt(prefix + card_number, OpenSSL::PKey::RSA::PKCS1_OAEP_PADDING)
 
-        # Base64 encode the resulting encrypted data into a string to Clover as the 'cardEncrypted' property.
-        card_encrypted = Base64.encode64(encrypted)
+            # Base64 encode the resulting encrypted data into a string to Clover as the 'cardEncrypted' property.
+            card_encrypted = Base64.encode64(encrypted)
 
-        # POST to /v2/merchant/{mId}/pay
-        post_data = {
-            "orderId": order_id,
-            # "tipAmount": tip_amount,
-            # "taxAmount": tax_amount,
-            "expMonth": exp_month,
-            "cvv": cvv,
-            "amount": amount,
-            "currency": "usd",
-            "last4": last4,
-            "expYear": exp_year,
-            "first6": first6,
-            "cardEncrypted": card_encrypted
-        }
+            # POST to /v2/merchant/{mId}/pay
+            post_data = {
+                "orderId": order_id,
+                # "tipAmount": tip_amount,
+                # "taxAmount": tax_amount,
+                "expMonth": exp_month,
+                "cvv": cvv,
+                "amount": amount,
+                "currency": "usd",
+                "last4": last4,
+                "expYear": exp_year,
+                "first6": first6,
+                "cardEncrypted": card_encrypted
+            }
 
-        begin
-            puts "Paying for order #{order_id} for shop_id: #{shop_id}"
-            pay_order_request = HTTP
-                .headers(
-                    :authorization => "Bearer #{access_token}",
-                    :content_type => "application/json",
-                )
-                .post(
-                    "#{pay_api_url}/#{merchant_id}/pay",
-                    :json => post_data
-                )
-            pay_resp = pay_order_request.parse
-        rescue HTTP::ResponseError => e
-            raise "Error on new order: #{e.message}"
+            begin
+                puts "Paying for order #{order_id} for shop_id: #{shop_id}"
+                pay_order_request = HTTP
+                    .headers(
+                        :authorization => "Bearer #{access_token}",
+                        :content_type => "application/json",
+                    )
+                    .post(
+                        "#{pay_api_url}/#{merchant_id}/pay",
+                        :json => post_data
+                    )
+                pay_resp = pay_order_request.parse
+            rescue HTTP::ResponseError => e
+                raise "Error on new order: #{e.message}"
 
-            render :json => {:error => JSON.parse(e.response_body)["errors"]}, :status => 400
-            return
-        end
+                render :json => {:error => JSON.parse(e.response_body)["errors"]}, :status => 400
+                return
+            end
 
-        if pay_resp["result"] == "APPROVED"
-            render json: {:status => 200, :data => {resp: pay_resp}}
+            if pay_resp["result"] == "APPROVED"
+                render json: {:status => 200, :data => {resp: pay_resp}}
+            else
+                render :json => {:message => "payment failed"}, :status => 400
+            end
         else
-            render :json => {:message => "payment failed"}, :status => 400
+            render :json => {:message => "shop not authorized", reset_app_state: true}, :status => 409
         end
     end
 
@@ -121,5 +130,31 @@ class Clover::Merchant::PayCloverController < ApplicationController
 
     def not_found
         raise ActionController::RoutingError.new('Not Found')
+    end
+
+    def is_rmote_shop_authorized(shop_id, access_token)
+        is_rmote_shop_authorized = true
+        begin
+            puts "Getting info for shop_id: #{shop_id}"
+            merchant_request = HTTP
+            .headers(:authorization => "Bearer #{access_token}")
+            .get("#{@clover_base_api_url}/v3/merchants/#{shop_id}?expand=address,openingHours")
+            merchant_resp = merchant_request.parse
+            if merchant_request.status == 401
+                if rejected_shop = Shop.find_by_remote_id(shop_id)
+                    is_rmote_shop_authorized = false
+                    Shop.delete(rejected_shop.id)
+                end
+            end
+
+        rescue HTTP::ResponseError => e
+            raise "Error encountered while getting access token: #{e.message}"
+
+            render :json => {:error => JSON.parse(e.response_body)["errors"]}, :status => 400
+            is_rmote_shop_authorized = false
+            is_rmote_shop_authorized
+        end
+
+        is_rmote_shop_authorized
     end
 end
